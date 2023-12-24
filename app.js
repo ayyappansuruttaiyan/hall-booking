85146f749f062e5e704883cefa7bb099601b06ff
ayyappansuruttaiyan <ayyappan.suruttaiyan@gmail.com>
2023-12-24 09:19:09 +0530

    api modified

--- app.js
+++ app.js
@@ -31,12 +31,12 @@ app.get("/", (req, res) => {
 });
 
 // List all available customers
-app.get("https://hallbooking-oqxt.onrender.com/api/customers", (req, res) => {
+app.get("/api/customers", (req, res) => {
   res.status(200).json(customers);
 });
 
 // Creating a room
-app.post("https://hallbooking-oqxt.onrender.com/api/rooms", (req, res) => {
+app.post("/api/rooms", (req, res) => {
   const { seats, amenities, price } = req.body;
   const room = {
     id: rooms.length + 1,
@@ -50,7 +50,7 @@ app.post("https://hallbooking-oqxt.onrender.com/api/rooms", (req, res) => {
 });
 
 // Booking a room
-app.post("https://hallbooking-oqxt.onrender.com/api/bookings", (req, res) => {
+app.post("/api/bookings", (req, res) => {
   const { customerName, date, startTime, endTime, roomId } = req.body;
   const room = rooms.find((r) => r.id === parseInt(roomId));
   if (!room) return res.status(404).json({ error: "Room not found" });
@@ -71,66 +71,57 @@ app.post("https://hallbooking-oqxt.onrender.com/api/bookings", (req, res) => {
 });
 
 // List all rooms with booked data
-app.get(
-  "https://hallbooking-oqxt.onrender.com/api/rooms/bookings",
-  (req, res) => {
-    const result = rooms.map((room) => {
-      const booking = bookings.find((b) => b.roomId === room.id);
-      return {
-        roomName: room.name,
-        booked: !!booking,
-        customerName: booking ? booking.customerName : null,
-        date: booking ? booking.date : null,
-        startTime: booking ? booking.startTime : null,
-        endTime: booking ? booking.endTime : null,
-      };
-    });
+app.get("/api/rooms/bookings", (req, res) => {
+  const result = rooms.map((room) => {
+    const booking = bookings.find((b) => b.roomId === room.id);
+    return {
+      roomName: room.name,
+      booked: !!booking,
+      customerName: booking ? booking.customerName : null,
+      date: booking ? booking.date : null,
+      startTime: booking ? booking.startTime : null,
+      endTime: booking ? booking.endTime : null,
+    };
+  });
 
-    res.status(200).json(result);
-  }
-);
+  res.status(200).json(result);
+});
 
 // List all customers with booked data
-app.get(
-  "https://hallbooking-oqxt.onrender.com/api/customers/bookings",
-  (req, res) => {
-    const result = bookings.map((booking) => {
-      const room = rooms.find((r) => r.id === booking.roomId);
-      return {
-        customerName: booking.customerName,
-        roomName: room.name,
-        date: booking.date,
-        startTime: booking.startTime,
-        endTime: booking.endTime,
-      };
-    });
+app.get("/api/customers/bookings", (req, res) => {
+  const result = bookings.map((booking) => {
+    const room = rooms.find((r) => r.id === booking.roomId);
+    return {
+      customerName: booking.customerName,
+      roomName: room.name,
+      date: booking.date,
+      startTime: booking.startTime,
+      endTime: booking.endTime,
+    };
+  });
 
-    res.status(200).json(result);
-  }
-);
+  res.status(200).json(result);
+});
 
 // List how many times a customer has booked the room
-app.get(
-  "https://hallbooking-oqxt.onrender.com/api/customers/booking-count",
-  (req, res) => {
-    const result = {};
-    bookings.forEach((booking) => {
-      const key = `${booking.customerName}_${booking.roomId}`;
-      result[key] = result[key] ? result[key] + 1 : 1;
-    });
+app.get("/api/customers/booking-count", (req, res) => {
+  const result = {};
+  bookings.forEach((booking) => {
+    const key = `${booking.customerName}_${booking.roomId}`;
+    result[key] = result[key] ? result[key] + 1 : 1;
+  });
 
-    const formattedResult = Object.entries(result).map(([key, count]) => {
-      const [customerName, roomId] = key.split("_");
-      return {
-        customerName,
-        roomId: parseInt(roomId),
-        count,
-      };
-    });
+  const formattedResult = Object.entries(result).map(([key, count]) => {
+    const [customerName, roomId] = key.split("_");
+    return {
+      customerName,
+      roomId: parseInt(roomId),
+      count,
+    };
+  });
 
-    res.status(200).json(formattedResult);
-  }
-);
+  res.status(200).json(formattedResult);
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
